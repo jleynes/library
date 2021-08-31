f4d204825f71a2bf4d4c7623850c1740cecb0708
jleynes <38470756+jleynes@users.noreply.github.com>
2021-08-31 16:44:16 -0700

    Updated files

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
 // Javascript
+let myLibrary = [];
+
+
+// DOM
+const table = document.getElementById("bookTable");
+
+
+// constructor for the book object
+function Book(name, author, pages, read) {
+    this.name = name;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    this.logBook = function() {
+        console.log(name);
+    }
+}
+
+// function that pushes book to library array
+function addBookToLibrary(name, author, pages, read) {
+    let b = new Book(name, author, pages, read);
+    myLibrary.push(b);
+}
+
+// function to add book to libary array
+// need to debug 
+function displayBooks() {
+  for (i = 0; i <= myLibrary.length; i++) {
+    let row = table.insertRow(i);
+    let cell = row.insertCell(i);
+
+    cell.innerHTML = JSON.stringify(myLibrary);
+  }
+}
